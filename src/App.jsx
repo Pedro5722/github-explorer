5570557a058e71332018a7b429dc7606c27c4634
João Pedro Albuquerque Araujo <pedro.araujo@grupomaster.local>
2022-05-18 17:03:31 -0300

    primiro commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+/* function tenHello(){
+    for (var i = 0; i < 10; i++){
+        <h1> hello World</h1>
+    }
+} */
+
+
+export function App(){
+    
+    return (
+        <div>
+            <h1>hello World</h1>
+        </div>
+        
+    
+    )
+    
+}
